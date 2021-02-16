2155b61ee28ee91a91d7cb411ff24649cd558751
apple <apple@apples-MacBook-Pro.local>
2021-02-16 01:08:31 +0100

    update index controller

--- src/main/java/com/ocheejeh/springmvc/controllers/IndexController.java
+++ src/main/java/com/ocheejeh/springmvc/controllers/IndexController.java
@@ -1,13 +1,21 @@
 package com.ocheejeh.springmvc.controllers;
 
+import com.ocheejeh.springmvc.services.CustomerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    CustomerService customerService;
 
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+    @RequestMapping("/customers")
+    public String showCustomers(Model model){
+        model.addAttribute("customers", customerService.getCustomers());
+        return "customers";
     }
 }
