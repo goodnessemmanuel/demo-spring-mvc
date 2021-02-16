e8130ee35d1420fd8b844aa98f024fe869a228ea
goodnessemmanuel <goodnessemmanuel@gmail.com>
2021-02-16 03:00:05 +0100

    added route to view a customer

--- src/main/java/com/ocheejeh/springmvc/controllers/IndexController.java
+++ src/main/java/com/ocheejeh/springmvc/controllers/IndexController.java
@@ -4,6 +4,7 @@ import com.ocheejeh.springmvc.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,9 +21,16 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
     @RequestMapping("/customers")
     public String showCustomers(Model model){
-        model.addAttribute("customers", customerService.getCustomers());
+        model.addAttribute("customers", customerService.listAllCustomers());
         return "customers";
+    }
+
+    @RequestMapping("/customer/{id}")
+    public String showCustomers(@PathVariable Integer id, Model model){
+        model.addAttribute("customer", customerService.getCustomerById(id));
+        return "customer";
     }
 }
